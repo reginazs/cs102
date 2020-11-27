502cb8c015b4c74cdefcb7d2ac16a7f20d92e48b
reginazs <dreghinak@gmail.com>
2020-11-27 11:20:11 +0300

    life.py & life_console.py

--- homework03/life.py
+++ homework03/life.py
@@ -29,46 +29,120 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        """
+        Создание списка клеток.
+
+        Клетка считается живой, если ее значение равно 1, в противном случае клетка
+        считается мертвой, то есть, ее значение равно 0.
+
+        Parameters
+        ----------
+        randomize : bool
+            Если значение истина, то создается матрица, где каждая клетка может
+            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
+
+        Returns
+        ----------
+        out : Grid
+            Матрица клеток размером `cell_height` х `cell_width`.
+        """
+        if randomize:
+            return [
+                [random.randint(0, 1) for x in range(self.cell_width)]
+                for _ in range(self.cell_height)
+            ]
+        else:
+            return [[0 for _ in range(self.cell_width)] for _ in range(self.cell_height)]
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+         """
+        Вернуть список соседних клеток для клетки `cell`.
+
+        Соседними считаются клетки по горизонтали, вертикали и диагоналям,
+        то есть, во всех направлениях.
+
+        Parameters
+        ----------
+        cell : Cell
+            Клетка, для которой необходимо получить список соседей. Клетка
+            представлена кортежем, содержащим ее координаты на игровом поле.
+
+        Returns
+        ----------
+        out : Cells
+            Список соседних клеток.
+        """
+        neighbors = []
+        shifts = ((i, j) for i in (-1, 2) for j in (-1, 2))
+        for x, y in shifts:
+            if (x, y) == (0, 0):
+                continue
+            row, col = cell[0] + x, cell[1] + y
+            if self._is_a_cell((row, col)):
+                neighbors.append(int(self.is_alive((row, col))))
+        return neighbors
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        """
+        Получить следующее поколение клеток.
+
+        Returns
+        ----------
+        out : Grid
+            Новое поколение клеток.
+        """
+        new_gen = self.create_grid(False)
+        for x in range(self.cell_height):
+            for y in range(self.cell_width):
+                new_ngbrs = self.get_neighbours((x, y)).count(1)
+                if self.grid[x][y] == 0 and new_ngbrs == 3:
+                    new_gen[x][y] = 1
+                elif self.grid[x][y] == 1 and new_ngbrs in [2, 3]:
+                    new_gen[x][y] = 1
+        return new_gen
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        if not self.is_max_generations_exceeded:
+        self.prev_generation = self.curr_generation()
+        self.curr_generation = self.get_next_generation()
+        self.generations +=1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        if not self.max_generations:
+            return False
+        return self.max_generations <= self.generations
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        return self.prev_generation != self.curr_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        with open(filename) as file:
+            grid = [[int(x) for x in list(rw)]] for rw in file.readline()]
+        row, col = len(grid), len(grid[0])
+
+        game = GameOfLife((row, col))
+        game.curr_generation = grid            
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+        with open(filename) as file:
+            for row in self.curr_generation:
+                file.write(''.join([str(x) for x in row]))
+                file.write('\n')
--- homework03/life_console.py
+++ homework03/life_console.py
@@ -1,4 +1,5 @@
 import curses
+import time
 
 from life import GameOfLife
 from ui import UI
@@ -10,13 +11,39 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        for x in range(self.life.cols + 2):
+            if x == 0 or x == self.life.cols + 1:
+                screen.addstr(0, x, "+")
+            else:
+                screen.addstr(0, x, "-")
+        for x in range(1, self.life.rows + 1):
+            screen.addstr(x, 0, "|")
+            screen.addstr(x, self.life.cols + 1, "|")
+        for x in range(self.life.cols + 2):
+            if x == 0 or x == self.life.cols + 1:
+                screen.addstr(self.life.rows + 1, x, "+")
+            else:
+                screen.addstr(self.life.rows + 1, x, "-")
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for x in range(1, self.rows - 1):
+            for y in range(1, self.cols - 1):
+                if self.life.curr_generation[x][y] == 1:
+                    screen.addstr(x + 1, y + 1, "*")
+                else:
+                    screen.addstr(x + 1, y + 1, " ")
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        self.draw_borders(screen)
+        self.draw_grid(screen)
+        screen.refresh()
+        time.sleep(1)
+        while not self.life.is_max_generations_exceed and self.life.is_changing:
+            self.life.step()
+            self.draw_grid(screen)
+            screen.refresh()
+            time.sleep(1)
+        screen.clear()
         curses.endwin()
