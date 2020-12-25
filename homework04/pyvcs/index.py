26bfec6db21c07045df3dddb5d841cfbb873fd51
reginazs <dreghinak@gmail.com>
2020-12-25 23:57:36 +0300

    helllpppplsss

--- homework04/pyvcs/index.py
+++ homework04/pyvcs/index.py
@@ -25,17 +25,76 @@ class GitIndexEntry(tp.NamedTuple):
     name: str
 
     def pack(self) -> bytes:
-        # PUT YOUR CODE HERE
+        return struct.pack(
+            "!10I20sh" + str(len(self.name)) + "s" + str(8 - (62 + len(self.name)) % 8) + "x",
+            self.ctime_s,
+            self.ctime_n,
+            self.mtime_s,
+            self.mtime_n,
+            self.dev,
+            self.ino & 0xFFFFFFFF,
+            self.mode,
+            self.uid,
+            self.gid,
+            self.size,
+            self.sha1,
+            self.flags,
+            self.name.encode(),
+        )
+
         ...
 
     @staticmethod
     def unpack(data: bytes) -> "GitIndexEntry":
-        # PUT YOUR CODE HERE
+        struct.unpacked("!LLLLLLLLLL20sH", data)
+        return GitIndexEntry(
+            unpacked[0],
+            unpacked[1],
+            unpacked[2],
+            unpacked[3],
+            unpacked[4],
+            unpacked[5],
+            unpacked[6],
+            unpacked[7],
+            unpacked[8],
+            unpacked[9],
+            unpacked[10],
+            unpacked[11],
+            unpacked[12].rstrip(b"\00").decode(),
+        )
         ...
 
 
 def read_index(gitdir: pathlib.Path) -> tp.List[GitIndexEntry]:
-    # PUT YOUR CODE HERE
+    index = []
+    if not (gitdir / "index").is_file():
+        return []
+    with open(gitdir / "index", "rb") as index_file:
+        data = index_file.read()
+    entry_count = struct.unpack("!i", data[8:12])[0]
+    data = data[12:]
+    for _ in range(entry_count):
+        entry = data[:60]
+        flags = data[60:62]
+        data = data[62:]
+        entry += flags
+        num_flags = int.from_bytes(flags, "big")
+        name = data[:num_flags].decode()
+        data = data[num_flags:]
+        entry += name.encode()
+        while True:
+            if len(data) == 0:
+                break
+            byte = chr(data[0])
+            if byte != "\x00":
+                break
+            entry += byte.encode("ascii")
+            data = data[1:]
+
+        entry_unpacked = GitIndexEntry.unpack(entry)
+        index.append(entry_unpacked)
+
+    return index
     ...
 
 
