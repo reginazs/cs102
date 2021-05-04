b50cbd6f4f2d5e55107528a8d95c7fdede81b7d7
reginazs <dreghinak@gmail.com>
2021-05-04 12:11:59 +0300

    cl

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,10 +1,10 @@
-import string
+from bottle import (
+    route, run, template, request, redirect
+)
 
-from bottle import redirect, request, route, run, template
-
-from bayes import NaiveBayesClassifie
+from scrapper import get_news
 from db import News, session
-from scraputils import get_news
+from bayes import NaiveBayesClassifier
 
 
 @route("/news")
@@ -16,56 +16,21 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    news_id = request.query.id
-    label = request.query.label
-    s = session()
-    qurent = s.query(News).filter(News.id == news_id).one()
-    qurent.label = label
-    s.commit()
+    # PUT YOUR CODE HERE
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    news = get_news('https://news.ycombinator.com/newest', 1)
-    s = session()
-    for item in news:
-        if s.query(News).filter(News.title == item['title'], News.author == item['author']).first():
-            continue
-        s.add(News(**item))
-
-    s.commit()
+    # PUT YOUR CODE HERE
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
-    def classify_news():
-    s = session()
-    rows_unlabelled = s.query(News).filter(News.label == None).all()
-    X = [clean(row.title).lower() for row in rows_unlabelled]
-
-    predictions = model.predict(X)
-    rows_good = [rows_unlabelled[i] for i in range(len(rows_unlabelled)) if predictions[i] == 'good']
-    rows_maybe = [rows_unlabelled[i] for i in range(len(rows_unlabelled)) if predictions[i] == 'maybe']
-    rows_never = [rows_unlabelled[i] for i in range(len(rows_unlabelled)) if predictions[i] == 'never']
-
-    return template('classify_template', rows_good=rows_good, rows_maybe=rows_maybe, rows_never=rows_never)
-
-
-def clean(s):
-    translator = str.maketrans("", "", string.punctuation)
-    return s.translate(translator).lower()
+    # PUT YOUR CODE HERE
 
 
 if __name__ == "__main__":
-    s = session()
-    rows = s.query(News).filter(News.label != None).all()
-    X_train = [clean(row.title).lower() for row in rows]
-    y_train = [row.label for row in rows]
-    model = NaiveBayesClassifier(alpha=0.05)
-    model.fit(X_train, y_train)
-
     run(host="localhost", port=8080)
-
 
