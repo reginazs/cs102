e2e264e0d1cc5ecc5b3b5ce8d832e514bb4c8bd2
reginazs <dreghinak@gmail.com>
2021-05-04 10:41:56 +0300

    hn

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,10 +1,10 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+import string
 
-from scrapper import get_news
+from bottle import redirect, request, route, run, template
+
+from bayes import NaiveBayesClassifie
 from db import News, session
-from bayes import NaiveBayesClassifier
+from scraputils import get_news
 
 
 @route("/news")
@@ -16,19 +16,41 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    news_id = request.query.id
+    label = request.query.label
+    s = session()
+    qurent = s.query(News).filter(News.id == news_id).one()
+    qurent.label = label
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    news = get_news('https://news.ycombinator.com/newest', 1)
+    s = session()
+    for item in news:
+        if s.query(News).filter(News.title == item['title'], News.author == item['author']).first():
+            continue
+        s.add(News(**item))
+
+    s.commit()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    def classify_news():
+    s = session()
+    rows_unlabelled = s.query(News).filter(News.label == None).all()
+    X = [clean(row.title).lower() for row in rows_unlabelled]
+
+    predictions = model.predict(X)
+    rows_good = [rows_unlabelled[i] for i in range(len(rows_unlabelled)) if predictions[i] == 'good']
+    rows_maybe = [rows_unlabelled[i] for i in range(len(rows_unlabelled)) if predictions[i] == 'maybe']
+    rows_never = [rows_unlabelled[i] for i in range(len(rows_unlabelled)) if predictions[i] == 'never']
+
+    return template('classify_template', rows_good=rows_good, rows_maybe=rows_maybe, rows_never=rows_never)
 
 
 if __name__ == "__main__":
